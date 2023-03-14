0edb58b30f33f80905032a40e7f43e8028604bfc
alexander_k <aleksandrkoronovskij@MacBook-Air-Aleksandr.local>
2023-03-14 12:57:55 +0500

    managers

--- src/apps/commons/manager.py
+++ src/apps/commons/manager.py
@@ -7,9 +7,17 @@ class ManagesBase:
 
     def create(self, instance):
         self.session.add(instance)
+        self.session.commit()
+        return instance
 
-    def update(self, instance):
+    def update(self, instance, data_update: dict):
+        for field in data_update.keys():
+            setattr(instance, field, data_update.get(field))
         self.session.add(instance)
+        self.session.commit()
+        return instance
 
     def delete(self, instance):
         self.session.delete(instance)
+        self.session.commit()
+        return instance
