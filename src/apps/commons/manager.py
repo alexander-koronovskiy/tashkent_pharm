a2aa017e24674927de2c9698e654058cf48d2a48
alexander_k <aleksandrkoronovskij@MacBook-Air-Aleksandr.local>
2023-03-15 19:25:10 +0500

    service raw

--- src/apps/commons/manager.py
+++ src/apps/commons/manager.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class ManagesBase:
+class ManagerBase:
     def __init__(self, session: AsyncSession):
         self.session = session
 
--- src/apps/commons/service.py
+++ src/apps/commons/service.py
@@ -1,11 +1,4 @@
-from db.models.users import *
-from db.models.company import *
-from db.models.arcticles import *
-from db.models.entity_legal import *
-from sqlalchemy import insert
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from apps.commons.manager import ManagerBase as manager_base
 
 
 class BaseService:
@@ -16,31 +9,42 @@ class BaseService:
         # Все запросы для вычитки должны брать за основу этот запрос.
         yield
 
-    def create(self, model):
-        # create Принимает на вход: Pydantic модель сущности
-        # Что делает: Создает в бд новую сущность с данными из переданной модели
-        # Возвращает: Инстанс модели
-        pass
+    async def create_post(self, model, data: dict):
+        """
+        Принимает на вход: Pydantic модель сущности
+        Что делает: Создает в бд новую сущность с данными из переданной модели
+        Возвращает: Инстанс модели
+        """
+        return await manager_base(model).create(data)
 
     def get(self, key: str, default: Any) -> Any:
-        # element attributes
-        if key in {'Id', 'Status'}:
-            return self._obj.attrib.get(key, default)
+        """
+        Принимает на вход: ID сущности
+        Что делает: Возвращает модель сущности с переданным ID
+        Возвращает: Инстанс модели
+        """
+        pass
 
     def filter(self):
-        # list Принимает на вход: Pydantic модель фильтра сущности
-        # Что делает: Возвращает список моделей сущностей подходящих под условия фильтрации
-        # Возвращает: Список инстансов модели
+        """
+        list Принимает на вход: Pydantic модель фильтра сущности
+        Что делает: Возвращает список моделей сущностей подходящих под условия фильтрации
+        Возвращает: Список инстансов модели
+        """
         pass
 
-    def update(self, model_id):
-        # update Принимает на вход: ID сущности, Pydantic модель для обновления
-        # Что делает: Обновляет сущность с переданным ID данными из Pydantic модели
-        # Возвращает: Инстанс модели
-        pass
+    def update(self, model_id, data: dict):
+        """
+        update Принимает на вход: ID сущности, Pydantic модель для обновления
+        Что делает: Обновляет сущность с переданным ID данными из Pydantic модели
+        Возвращает: Инстанс модели
+        """
+        return await manager_base(model_id).update(data)
 
-    def delete(self):
-        # delete Принимает на вход: ID сущности
-        # Что делает: Удаляет (мягко или окончательно) сущность из базы
-        # Возвращает: Ничего
-        pass
+    def delete(self, model_id, data: dict):
+        """
+        delete Принимает на вход: ID сущности
+        Что делает: Удаляет (мягко или окончательно) сущность из базы
+        Возвращает: Ничего
+        """
+        return await manager_base(model_id).delete()
