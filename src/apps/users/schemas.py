885f4ef4a9e716456849e1e121abc8de66b568a5
alexander_k <aleksandrkoronovskij@MacBook-Air-Aleksandr.local>
2023-03-20 16:30:19 +0500

    meeting 20/3

--- src/apps/users/schemas.py
+++ src/apps/users/schemas.py
@@ -1,14 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 
 
 class UserCreate(BaseModel):
-    pass
+    name_first: constr(max_length=50)
+    name_middle: constr(max_length=50)
+    name_last: constr(max_length=50)
+
+    id_company: int
+
 # class UserUpdate
 # class UserFilter
 # class UserShort
 
 
 class UserDetailed:
-    pass
+    name_first: constr(max_length=50)
+    name_middle: constr(max_length=50)
+    name_last: constr(max_length=50)
+
+    id_company: int
+
+    class Config:
+        orm_mode = True
+
 
 # при фильтрации все поля опциональны
