ff906ea3a6e63a351b4616118cf5908bef43b7e7
alexander_k <aleksandrkoronovskij@MacBook-Air-Aleksandr.local>
2023-03-21 13:19:04 +0500

    fix imort service

--- src/apps/users/services.py
+++ src/apps/users/services.py
@@ -1,7 +1,6 @@
 from apps.commons.service import ServiceBase
 from db.models.users import User
 from db.models.entity_legal import EntityLegal
-from db.models.pharmacy import Pharmacy
 from db.models.arcticles import Arcticle
 
 
