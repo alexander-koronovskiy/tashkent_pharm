30a0ffda9ba291310e8db48e4a7d656125f6475c
alexander_k <aleksandrkoronovskij@MacBook-Air-Aleksandr.local>
2023-03-21 14:36:23 +0500

    pharmacy debug

--- src/api/pharmacy.py
+++ src/api/pharmacy.py
@@ -1,3 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from apps.commons.manager import ManagerBase
+from apps.pharmacy.schemas import PharmacyDetailed, PharmacyCreate
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from apps.pharmacy.services import ServicePharmacy
+from db.database import get_session
 
 router = APIRouter(prefix='/pharmacy', tags=['Pharmacy'])
+
+
+@router.post(
+    path='',
+    name='Create Pharmacy',
+    operation_id='create_pharmacy',
+    response_model=PharmacyDetailed,
+    tags=['Pharmacy'],
+    status_code=201,
+)
+async def save(
+        data: PharmacyCreate,
+        session: AsyncSession = Depends(get_session)
+) -> PharmacyDetailed:
+    manager = ManagerBase(session)
+    service = ServicePharmacy(manager)
+    return await service.create(data)
