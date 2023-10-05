dc7cb971ee06c038d28a6e766837b1c94b969e7e
alexander_k <aleksandrkoronovskij@MacBook-Air-Aleksandr.local>
2023-10-05 19:20:49 +0300

    pharmacy/services change

--- src/apps/pharmacy/services.py
+++ src/apps/pharmacy/services.py
@@ -1,4 +1,5 @@
 from apps.commons.service import ServiceBase
+from apps.pharmacy.schemas import PharmacyCreate, PharmacyFilter
 from db.models.company import Company
 
 
@@ -6,4 +7,9 @@ class ServicePharmacy(ServiceBase):
     MODEL = Company
 
     # переопределить метод create -> созд компании тип проставить
+    async def create(self, data: PharmacyCreate) -> Company:
+        return await self.create(data)
+
     # переопределить select -> выбирал только аптеки
+    async def select(self, data: PharmacyFilter) -> Company:
+        return await self.create(data)
